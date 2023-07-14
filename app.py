fda8391443fb075336437daa13c2290d845b4cba
Symon Barreto <contatosymon@gmail.com>
2023-07-14 02:14:47 -0400

    FEAT:Products table at products page

--- app.py
+++ app.py
@@ -8,4 +8,10 @@ def home_page():
 
 @app.route('/products')
 def products_page():
-    return render_template("products.html")
+    products = [
+        {'id': 1, 'name': 'cellphone', 'barcode': '123789', 'price': 1200},
+        {'id': 2, 'name': 'laptop', 'barcode': '113784', 'price': 2400},
+        {'id': 3, 'name': 'fridge', 'barcode': '125186', 'price': 1650},
+        {'id': 4, 'name': 'television', 'barcode': '163719', 'price': 3000}
+    ]
+    return render_template("products.html", itens=products)
