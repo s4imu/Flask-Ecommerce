8e45521b5f5e5b13294ca9632d46444d16084f3f
Symon Barreto <contatosymon@gmail.com>
2023-07-21 15:25:58 -0400

    FEAT: Register user from form to DB

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app 
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
 
 @app.route('/')
 def home_page():
@@ -16,7 +17,17 @@ def products_page():
 def login_page(): 
     return render_template("login.html")
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        new_user = User(
+            user = form.user.data,
+            email = form.email.data,
+            password = form.password1.data
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('products_page'))
+
     return render_template("signup.html", form=form)
