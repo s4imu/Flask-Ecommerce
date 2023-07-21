58e2e82b05c57b5bcefbf7f791040d144520de8d
Symon Barreto <contatosymon@gmail.com>
2023-07-21 01:18:51 -0400

    FEAT: integrating database with front end

--- market.py
+++ market.py
@@ -22,12 +22,7 @@ def home_page():
 
 @app.route('/products')
 def products_page():
-    products = [
-        {'id': 1, 'name': 'cellphone', 'barcode': '123789', 'price': 1200},
-        {'id': 2, 'name': 'laptop', 'barcode': '113784', 'price': 2400},
-        {'id': 3, 'name': 'fridge', 'barcode': '125186', 'price': 1650},
-        {'id': 4, 'name': 'television', 'barcode': '163719', 'price': 3000}
-    ]
+    products = Item.query.all()
     return render_template("products.html", itens=products)
 
 
