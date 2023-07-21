b62fa7dd56af6a4947e0381a9d978686876b8a18
Symon Barreto <contatosymon@gmail.com>
2023-07-21 00:53:21 -0400

    FEAT: Itens table on DB

--- market.py
+++ market.py
@@ -4,8 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 app = Flask(__name__)
-app.config["SQL_ALCHEMY_DATABASE_URI"]="sqlite:///market.db"
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///market.db"
 db.init_app(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False)
 
 @app.route('/')
 def home_page():
