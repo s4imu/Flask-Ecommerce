f085ea8d19364b2adebca5e35b55e763411d20af
Symon Barreto <contatosymon@gmail.com>
2023-07-22 00:35:54 -0400

    FEAT: Checking if username and/or email already in use

--- market/forms.py
+++ market/forms.py
@@ -1,11 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
 
 class RegisterForm(FlaskForm):
+    def validate_username(self, check_user):
+        user = User.query.filter_by(user=check_user.data).first()
+        if user:
+            raise ValidationError("Username already in use! Please use another username.")
+
+    def validate_email(self, check_email):
+        email = User.query.filter_by(email=check_email.data).first()
+        if email:
+            raise ValidationError("Email already in use! Please use another email.")
+
     user = StringField(label='Username:', validators=[Length(min=2, max=30), DataRequired()])
     email = StringField(label='Email:', validators=[Email(), DataRequired()])
     password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Password Confirmation:', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Register')
-
